90dcf3b26dad6d0da92970d971fc6feac9f7b6f6
Robel Effa <96461347+Rexpass@users.noreply.github.com>
2023-10-17 16:26:28 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,55 @@
-#!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
+#!/usr/bin/env python3
 
 import sys
+import signal
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+# Initialize variables to store metrics
+total_file_size = 0
+status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+def print_statistics():
+    print(f"File size: {total_file_size}")
+    for status_code in sorted(status_code_counts.keys()):
+        count = status_code_counts[status_code]
+        if count > 0:
+            print(f"{status_code}: {count}")
+
+def signal_handler(sig, frame):
+    print_statistics()
+    sys.exit(0)
+
+# Register the signal handler for CTRL+C
+signal.signal(signal.SIGINT, signal_handler)
 
 try:
+    line_count = 0
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+        line_count += 1
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+        # Parse the line
+        try:
+            parts = line.split()
+            ip_address = parts[0]
+            status_code = int(parts[-2])
+            file_size = int(parts[-1])
 
-except Exception as err:
-    pass
+            # Check if the line format is valid
+            if len(parts) < 12 or parts[5] != '"GET' or parts[6] != '/projects/260' or parts[8] != 'HTTP/1.1"':
+                continue
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+            # Update metrics
+            total_file_size += file_size
+            status_code_counts[status_code] += 1
+
+            # Print statistics every 10 lines
+            if line_count % 10 == 0:
+                print_statistics()
+
+        except (ValueError, IndexError):
+            # Ignore lines with invalid format
+            continue
+
+except KeyboardInterrupt:
+    # Handle keyboard interruption
+    print_statistics()
+    sys.exit(0)
